3f76c16343b571ab7942fb25441b6f1aebeba0d4
vanthin29112000 <58946946+vanthin29112000@users.noreply.github.com>
2023-04-06 00:42:33 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,6 @@ async function bootstrap() {
 //     credentials: true,
 //   });
 
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-        next();
-    });
 
     app.enableCors({
         allowedHeaders:"*",
