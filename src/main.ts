f129a6c88fe4147d0e604203b93b96f9a3fac538
vanthin29112000 <58946946+vanthin29112000@users.noreply.github.com>
2023-04-06 00:34:08 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://voucher-hunter-app.netlify.app', 'http://localhost:3000'],
+    origin: ['https://voucher-hunter-app.netlify.app/', 'http://localhost:3000'],
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
     credentials: true,
   });
