7b789df6f8293ed134a430c3cfd2571794d763cb
vanthin29112000 <58946946+vanthin29112000@users.noreply.github.com>
2023-04-06 00:35:44 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,27 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://voucher-hunter-app.netlify.app/', 'http://localhost:3000'],
-    methods: ['POST', 'PUT', 'DELETE', 'GET'],
-    credentials: true,
-  });
+//   app.enableCors({
+//     origin: ['https://voucher-hunter-app.netlify.app/', 'http://localhost:3000'],
+//     methods: ['POST', 'PUT', 'DELETE', 'GET'],
+//     credentials: true,
+//   });
+   app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('hbs');
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+        next();
+    });
+
+    app.enableCors({
+        allowedHeaders:"*",
+        origin: "*"
+    });
+
   await app.listen(3003);
 }
 bootstrap();
